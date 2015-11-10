ea4c44e182d23d65ddcbf847001866784021c3fc
Morgan Townsend <morgantownsend@morgans-mbp.lan>
2015-11-10 13:26:45 +0800

    Initial Commit

---
+++ Supercool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Supercool
+//
+//  Created by Morgan Townsend on 2015-11-10.
+//  Copyright © 2015 Morgan Townsend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
